b6e69116df8537193b25c87387d9261f2097eda5
Mathulsh <Mathulsh@buaa.edu.cn>
2026-03-19 19:48:02 +0800

    更新redis实例

--- data_to_duckdb.py
+++ data_to_duckdb.py
@@ -1,12 +1,21 @@
 import redis
 from rd3 import collect_redis_results_to_duckdb
-
+# 服务器Redis实例
 redis_list = [
-    redis.Redis(host="127.0.0.1", port=6379, decode_responses=False),
-    redis.Redis(host="127.0.0.1", port=6380, decode_responses=False),
-    redis.Redis(host="127.0.0.1", port=6381, decode_responses=False),
-    redis.Redis(host="127.0.0.1", port=6382, decode_responses=False),
+    redis.Redis(host="10.64.199.25", port=41882, decode_responses=False),
+    redis.Redis(host="10.64.199.63", port=41883, decode_responses=False),
+    redis.Redis(host="10.64.199.62", port=41884, decode_responses=False),
+    redis.Redis(host="10.64.199.26", port=41885, decode_responses=False),
 ]
+
+# 本机Redis实例
+# redis_list = [
+#     redis.Redis(host="127.0.0.1", port=6379, decode_responses=False),
+#     redis.Redis(host="127.0.0.1", port=6380, decode_responses=False),
+#     redis.Redis(host="127.0.0.1", port=6381, decode_responses=False),
+#     redis.Redis(host="127.0.0.1", port=6382, decode_responses=False),
+# ]
+
 collect_redis_results_to_duckdb(
     # redis_host=redis_host,
     # redis_port=redis_port,
--- rd3.py
+++ rd3.py
@@ -2,16 +2,20 @@
 import redis, pandas as pd
 import pickle, time, duckdb
 
-# redis_host: str = "10.64.199.70"
-# redis_port: int = 41528
-# redis_password: str | None = None
-
+# 服务器Redis实例
 redis_list = [
-    redis.Redis(host="127.0.0.1", port=6379, decode_responses=False),
-    redis.Redis(host="127.0.0.1", port=6380, decode_responses=False),
-    redis.Redis(host="127.0.0.1", port=6381, decode_responses=False),
-    redis.Redis(host="127.0.0.1", port=6382, decode_responses=False),
+    redis.Redis(host="10.64.199.25", port=41882, decode_responses=False),
+    redis.Redis(host="10.64.199.63", port=41883, decode_responses=False),
+    redis.Redis(host="10.64.199.62", port=41884, decode_responses=False),
+    redis.Redis(host="10.64.199.26", port=41885, decode_responses=False),
 ]
+# 本机Redis实例
+# redis_list = [
+#     redis.Redis(host="127.0.0.1", port=6379, decode_responses=False),
+#     redis.Redis(host="127.0.0.1", port=6380, decode_responses=False),
+#     redis.Redis(host="127.0.0.1", port=6381, decode_responses=False),
+#     redis.Redis(host="127.0.0.1", port=6382, decode_responses=False),
+# ]
 idx = 0
 
 def push_to_redis(combs):
